9c64e9e9eefeea44dce689da8ee23d4fed1fc989
mac <mac@Apples-MacBook-Pro.local>
2023-06-19 00:14:02 +0530

    generate migrations

--- Project_Authentication/Project_Authentication/settings.py
+++ Project_Authentication/Project_Authentication/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # installed app
+    'app_enroll.apps.AppEnrollConfig',
 ]
 
 MIDDLEWARE = [
